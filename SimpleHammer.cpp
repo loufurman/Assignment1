66750f0cdba8ba00ad5cc391b031abdff86ef201
Brendan Schneider <bzs14@gmail.com>
2019-02-03 15:38:34 -0500

    Add SimpleHammer.cpp
    25 hitpoints, and ignores armor if armor < 30. Has not yet been tested
    in main.cpp

---
+++ SimpleHammer.cpp
@@ -0,0 +1,33 @@
+//Brendan Schneider - bzs14
+//Assignment 1 - git
+
+#include <string>
+#include "Weapon.h"
+
+//The Hammer class (25HP, ignore armor < 30)
+class SimpleHammer : public Weapon
+{
+public:
+
+    SimpleHammer() : Weapon("Simple Hammer", 25.0) {};
+
+    virtual ~SimpleHammer() {};
+
+    virtual double hit(double armor);
+
+};
+
+double SimpleHammer::hit(double armor)
+{
+    double damage;
+	if (armor >= 30)
+		damage = hitPoints - armor;
+	else
+		damage = hitPoints;
+	
+	
+    if(damage < 0)
+        return 0;
+	else
+		return damage;
+}
